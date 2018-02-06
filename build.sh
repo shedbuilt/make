379d85315e63193a165ac775980a7fca23cb5d5d
Auston Stewart <auston@shedbuilt.net>
2018-02-05 14:51:31 -1000

    Fixing compilation issues with glibc 2.27

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# Workaround for compilation issue with glibc 2.27
+sed -i '211,217 d; 219,229 d; 232 d' glob/glob.c
 case "$SHED_BUILDMODE" in
     toolchain)
         ./configure --prefix=/tools \
-                    --without-guile || return 1
+                    --without-guile || exit 1
         ;;
     *)
-        ./configure --prefix=/usr || return 1
+        ./configure --prefix=/usr || exit 1
         ;;
 esac
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" install || return 1
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install
