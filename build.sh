bf8650466704d4bf22b787cbc30241b1acea6bbd
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:03:19 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Workaround for compilation issue with glibc 2.27
-sed -i '211,217 d; 219,229 d; 232 d' glob/glob.c
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        ./configure --prefix=/tools \
-                    --without-guile || exit 1
-        ;;
-    *)
-        ./configure --prefix=/usr || exit 1
-        ;;
-esac
-make -j $SHED_NUM_JOBS && \
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+SHED_PKG_LOCAL_PREFIX='/usr'
+SHED_PKG_LOCAL_GUILE_OPTION=''
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_PREFIX='/tools'
+    SHED_PKG_LOCAL_GUILE_OPTION='--without-guile'
+fi
+sed -i '211,217 d; 219,229 d; 232 d' glob/glob.c &&
+./configure --prefix=${SHED_PKG_LOCAL_PREFIX} ${SHED_PKG_LOCAL_GUILE_OPTION} &&
+
+# Build and Install
+make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
